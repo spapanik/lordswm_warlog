65e0644e24fa8f36b8f4fbb986806738bf23b3e4
Stephanos Kuma <stephanos@kuma.ai>
2021-11-19 15:03:57 +0000

    Initial commit

---
+++ lordswm_warlog.js
@@ -0,0 +1,23 @@
+// ==UserScript==
+// @name         Lordswm warlog
+// @namespace    https://openuserjs.org/users/spapanik
+// @version      0.1.1
+// @description  Link to full battle
+// @author       spapanik
+// @match        https://www.lordswm.com/*
+// @licence      LGPL-3.0-or-later
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    let hyperlinks = document.getElementsByTagName( 'a' );
+    let i;
+
+    for (i=0; i < hyperlinks.length; i++) {
+        let element = hyperlinks[i];
+        if (element.href.indexOf('/warlog.php?') != -1) {
+            element.href = element.href + '&lt=-1';
+        }
+    }
+})();
